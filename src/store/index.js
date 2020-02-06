308ba2d1797af0b50950a1e81a5c2e726270aa6e
Joey Schroeder <schrjos@gmail.com>
2020-02-06 16:55:42 -0600

    Add request-tracking reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { locationReducer, REDUCER_PREFIX as LOCATION } from './location/location';
 import { currentWeatherReducer, REDUCER_PREFIX as CURRENT_WEATHER } from './current-weather/current-weather';
+import { requestTrackingReducer, REDUCER_PREFIX as REQUEST_TRACKING } from './request-tracking/request-tracking';
 
 export const reducers = combineReducers({
   [CURRENT_WEATHER]: currentWeatherReducer,
-  [LOCATION]: locationReducer
+  [LOCATION]: locationReducer,
+  [REQUEST_TRACKING]: requestTrackingReducer
 });
---
+++ src/store/request-tracking/request-tracking.js
@@ -0,0 +1,49 @@
+import { REQUEST_ACTION_TYPES_SUFFIXES } from '../../constants/request-action-types-suffixes';
+import { REQUEST_TRACKING_STATES } from '../../constants/request-tracking-states';
+
+const SUFFIX_TO_STATE_TRANSLATIONS = {
+  [REQUEST_ACTION_TYPES_SUFFIXES.FAILURE]: REQUEST_TRACKING_STATES.FAILURE,
+  [REQUEST_ACTION_TYPES_SUFFIXES.START]: REQUEST_TRACKING_STATES.RUNNING,
+  [REQUEST_ACTION_TYPES_SUFFIXES.SUCCESS]: REQUEST_TRACKING_STATES.SUCCESS
+};
+
+const suffixRegexPattern = `(.*)_(${REQUEST_ACTION_TYPES_SUFFIXES.START}|${REQUEST_ACTION_TYPES_SUFFIXES.SUCCESS}|${REQUEST_ACTION_TYPES_SUFFIXES.FAILURE})`;
+const suffixRegex = new RegExp(suffixRegexPattern);
+
+export const REDUCER_PREFIX = 'request-tracking';
+
+// default state
+const DEFAULT_STATE = {};
+
+// reducer
+export const requestTrackingReducer = (state = {}, action = {}) => {
+  const { type } = action;
+  const actionTypeMatches = suffixRegex.exec(type);
+
+  if (!actionTypeMatches) return state;
+
+  const [, requestPrefix, requestSuffix] = actionTypeMatches;
+  const newState = SUFFIX_TO_STATE_TRANSLATIONS[requestSuffix] || REQUEST_TRACKING_STATES.UNTRACKED;
+
+  return {
+    ...state,
+    [requestPrefix]: newState
+  };
+};
+
+// selectors
+export const selectRequestTracking = (state = {}) => state[REDUCER_PREFIX] || DEFAULT_STATE;
+export const selectRequestTrackingByRequestPrefix = (state, requestPrefix) =>
+  selectRequestTracking(state)[requestPrefix] || REQUEST_TRACKING_STATES.UNTRACKED;
+
+export const selectRequestIsRunning = (state, requestPrefix) =>
+  selectRequestTrackingByRequestPrefix(state)[requestPrefix] === REQUEST_TRACKING_STATES.RUNNING;
+export const selectRequestHasSucceeded = (state, requestPrefix) =>
+  selectRequestTrackingByRequestPrefix(state)[requestPrefix] === REQUEST_TRACKING_STATES.SUCCESS;
+export const selectRequestHasFailed = (state, requestPrefix) =>
+  selectRequestTrackingByRequestPrefix(state)[requestPrefix] === REQUEST_TRACKING_STATES.FAILURE;
+
+export const selectRequestsHaveAllSucceeded = (state, requestPrefixes = []) =>
+  requestPrefixes.every(requestKey => selectRequestHasSucceeded(state, requestKey));
+
+// thunks
