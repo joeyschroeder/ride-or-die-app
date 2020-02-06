5166cbcbf0269ff0dba9dfc4194061803e342193
Joey Schroeder <schrjos@gmail.com>
2020-02-05 20:58:00 -0600

    Add weather service

---
+++ src/constants/open-weather-api-root-url.js
@@ -0,0 +1,1 @@
+export const OPEN_WEATHER_API_ROOT_URL = 'http://samples.openweathermap.org/data/2.5';
---
+++ src/services/weather/weather.js
@@ -0,0 +1,22 @@
+import { get } from 'axios';
+import { OPEN_WEATHER_API_ROOT_URL } from '../../constants/open-weather-api-root-url';
+import { OPEN_WEATHER_API_KEY } from '../../constants/open-weather-api-key';
+
+export const WEATHER_ENDPOINT = `${OPEN_WEATHER_API_ROOT_URL}/weather`;
+
+export const getWeatherByGeographicCoordinates = async ({ latitude: lat, longitude: lon } = {}) => {
+  if (!lat) throw new Error('Latitude is undefined.');
+  if (!lon) throw new Error('Longitude is undefined.');
+
+  const response = await get(WEATHER_ENDPOINT, {
+    params: {
+      appid: OPEN_WEATHER_API_KEY,
+      lat,
+      lon
+    }
+  });
+
+  const { data } = response;
+
+  return data;
+};
