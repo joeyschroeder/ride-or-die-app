c85c7bcea46d84ea960b15d545e2b9efdee0cc6f
Joey Schroeder <schrjos@gmail.com>
2020-02-06 19:45:58 -0600

    Update settings reducer

--- src/store/settings/settings.js
+++ src/store/settings/settings.js
@@ -1,10 +1,19 @@
+import { createAction, handleActions } from 'redux-actions';
+
 import { TEMPERATURE_MEASUREMENTS } from '../../constants/temperature-measurements';
-import { createRequestReducer } from '../../util/create-request-reducer/create-request-reducer';
+import { createRequestActionSuccessType } from '../../util/create-request-action-success-type/create-request-action-success-type';
 import { createRequestThunk } from '../../util/create-request-thunk/create-request-thunk';
 import { getAsyncStorageItem } from '../../services/async-storage/async-storage';
 
 export const REDUCER_PREFIX = 'settings';
 const REQUEST_PREFIX = `${REDUCER_PREFIX}/REQUEST`;
+
+// action types
+const REQUEST_SUCCESS = createRequestActionSuccessType(REQUEST_PREFIX);
+const UPDATE_TEMPERATURE_MEASUREMENT = `${REDUCER_PREFIX}/UPDATE`;
+
+// action creators
+export const updateSettingsTemperatureMeasurement = createAction(UPDATE_TEMPERATURE_MEASUREMENT);
 
 // default state
 const DEFAULT_STATE = {
@@ -12,7 +21,16 @@ const DEFAULT_STATE = {
 };
 
 // reducer
-export const settingsReducer = createRequestReducer(REQUEST_PREFIX, DEFAULT_STATE);
+export const settingsReducer = handleActions(
+  {
+    [REQUEST_SUCCESS]: (state, { payload }) => payload || state,
+    [UPDATE_TEMPERATURE_MEASUREMENT]: (state, { payload: temperatureMeasurement }) => ({
+      ...state,
+      temperatureMeasurement
+    })
+  },
+  DEFAULT_STATE
+);
 
 // selectors
 export const selectSettings = (state = {}) => state[REDUCER_PREFIX] || DEFAULT_STATE;
