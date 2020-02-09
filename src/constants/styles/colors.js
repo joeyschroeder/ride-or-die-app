ec5e361293e2e71f24d53233cca146b1b9ca34e9
Joey Schroeder <schrjos@gmail.com>
2020-02-09 17:34:37 -0600

    Add color constants

---
+++ src/constants/styles/colors.js
@@ -0,0 +1,13 @@
+import Color from 'color';
+
+export const COLORS = {
+  BLACK: Color('#000'),
+  BLACK_TYPE: Color('#333132'),
+  DANGER: Color('#ff0054'),
+  GREY: Color('#333132').lighten(3.2),
+  SUCCESS: Color('#18dc87'),
+  TRANSPARENT: 'transparent',
+  TWITTER: Color('#6cadde'),
+  WARNING: Color('#ffbb37'),
+  WHITE: Color('#fff')
+};
