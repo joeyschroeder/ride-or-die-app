9c43249a6131e93d80f5d259abd0f37501669a01
Joey Schroeder <schrjos@gmail.com>
2020-02-15 13:26:02 -0600

    Alphabetize props on getTimeOfDay util

--- src/util/get-time-of-day/get-time-of-day.js
+++ src/util/get-time-of-day/get-time-of-day.js
@@ -1,6 +1,6 @@
 import { TIMES_OF_DAY } from '../../constants/times-of-day';
 
-export const getTimeOfDay = ({ time, sunrise, sunset }) => {
+export const getTimeOfDay = ({ sunrise, sunset, time }) => {
   if (time < sunrise) return TIMES_OF_DAY.NIGHT;
   if (time < sunset) return TIMES_OF_DAY.DAY;
 
