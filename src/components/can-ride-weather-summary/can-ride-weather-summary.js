6fa7e5c7dbfa2dc6f2c350a3658ef4351c59b026
Joey Schroeder <schrjos@gmail.com>
2020-02-15 11:46:21 -0600

    Add icon to canRideWeatherSummary

--- src/components/can-ride-weather-summary/can-ride-weather-summary.js
+++ src/components/can-ride-weather-summary/can-ride-weather-summary.js
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, ViewPropTypes } from 'react-native';
+import { StyleSheet, Text, View, ViewPropTypes } from 'react-native';
 
 import { COLORS } from '../../constants/styles/colors';
 import { FONTS } from '../../constants/styles/fonts';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { SPACER } from '../../constants/styles/spacer';
 import { SlideInUpFadeAnimation } from '../slide-in-up-fade-animation/slide-in-up-fade-animation';
 import { TemperatureConnected } from '../temperature/temperature.connected';
 import { scaledLineHeight } from '../../util/scaled-line-height/scaled-line-height';
@@ -13,6 +14,17 @@ const styles = StyleSheet.create({
   city: {
     color: COLORS.WHITE,
     fontFamily: FONTS.SANS_SERIF.MEDIUM
+  },
+  icon: {
+    color: COLORS.WHITE,
+    fontFamily: FONTS.WEATHER_ICONS,
+    fontSize: scaledValue(50)
+  },
+  iconContainer: {
+    marginRight: SPACER
+  },
+  root: {
+    flexDirection: 'row'
   },
   temperature: {
     color: COLORS.WHITE,
@@ -24,11 +36,17 @@ const styles = StyleSheet.create({
 
 export const CanRideWeatherSummary = props => {
   const { city, temperature, style } = props;
+  const rootStyle = { ...style, ...styles.root };
 
   return (
-    <SlideInUpFadeAnimation animateOnUpdate style={style}>
-      <TemperatureConnected style={styles.temperature} value={temperature} />
-      <Text style={styles.city}>{city.toUpperCase()}</Text>
+    <SlideInUpFadeAnimation animateOnUpdate style={rootStyle}>
+      <View style={styles.iconContainer}>
+        <Text style={styles.icon}>{'\uF001'}</Text>
+      </View>
+      <View>
+        <TemperatureConnected style={styles.temperature} value={temperature} />
+        <Text style={styles.city}>{city.toUpperCase()}</Text>
+      </View>
     </SlideInUpFadeAnimation>
   );
 };
