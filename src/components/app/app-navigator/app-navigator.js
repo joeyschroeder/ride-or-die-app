ba293def6f5597bd6f0011e0b631281027d2dd4a
Joey Schroeder <schrjos@gmail.com>
2020-03-28 11:58:19 -0500

    Add constant for app route names

--- src/components/app/app-navigator/app-navigator.js
+++ src/components/app/app-navigator/app-navigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { Settings } from '../../settings/settings';
 import { createStackNavigator } from '@react-navigation/stack';
+import { APP_ROUTE_NAMES } from '../../../constants/app-route-names';
 
 const StackNavigator = createStackNavigator();
 const { Navigator, Screen } = StackNavigator;
@@ -10,9 +11,9 @@ const { Navigator, Screen } = StackNavigator;
 export const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Navigator headerMode="none">
-        <Screen name="Can Ride Current Weather" component={CanRideCurrentWeatherConnected} />
-        <Screen name="Settings" component={Settings} />
+      <Navigator headerMode="node">
+        <Screen name={APP_ROUTE_NAMES.CAN_RIDE_CURRENT_WEATHER} component={CanRideCurrentWeatherConnected} />
+        <Screen name={APP_ROUTE_NAMES.SETTINGS} component={Settings} />
       </Navigator>
     </NavigationContainer>
   );
---
+++ src/constants/app-route-names.js
@@ -0,0 +1,4 @@
+export const APP_ROUTE_NAMES = {
+  CAN_RIDE_CURRENT_WEATHER: 'Can Ride Current Weather',
+  SETTINGS: 'Settings'
+};
